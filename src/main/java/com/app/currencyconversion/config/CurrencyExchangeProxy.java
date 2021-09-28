9c4b8f2248523eeaf671edce21d5e6ce0d6eec63
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-09-28 07:53:17 +0530

    updated proxy

--- src/main/java/com/app/currencyconversion/config/CurrencyExchangeProxy.java
+++ src/main/java/com/app/currencyconversion/config/CurrencyExchangeProxy.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @author t0k02w6 on 26/09/21
  * @project currency-conversion
  */
-@FeignClient(name = "currency-exchange", url = "localhost:8083")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
